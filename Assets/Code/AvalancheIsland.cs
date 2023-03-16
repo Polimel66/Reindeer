a4b759c34c3d469f9ebb660cda8d588d5203678d
Polimel66 <melnikowapolia@yandex.ru>
2023-03-16 10:49:24 -0700

    Вариант лавины

---
+++ Assets/Code/AvalancheIsland.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheIsland : MonoBehaviour
+{
+    public GameObject pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8;
+    public float speed;
+    public Transform startPos;
+    Vector3 nextPos;
+    GameObject[] positionQueue;
+    public bool isLavinaStart;
+    int indexOfPosition;
+    void Start()
+    {
+        nextPos = startPos.position;
+        positionQueue = new GameObject[] { pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8};
+        indexOfPosition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLavinaStart)
+        {
+            if (indexOfPosition < positionQueue.Length - 1 && transform.position == positionQueue[indexOfPosition].transform.position)
+            {
+                indexOfPosition += 1;
+                nextPos = positionQueue[indexOfPosition].transform.position;
+            }
+            else if(indexOfPosition == positionQueue.Length)
+            {
+                isLavinaStart = false;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+
+            }
+        }
+    }
+}
