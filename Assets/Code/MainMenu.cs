9b2710584e5cf052dbf9e43d0f8ef3c54b98d05d
UZle55 <Anton.Korelin@urfu.me>
2023-04-29 21:28:44 +0500

    Добавил экран загрузки

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject continueButton;
+    public GameObject loadingScreen;
+    public GameObject loadingProgressBar;
     void Start()
     {
         if (SaveManager.isAnySaves())
@@ -27,15 +30,17 @@ public class MainMenu : MonoBehaviour
 
     public void OnContinueGameButtonClick()
     {
-        SceneManager.LoadScene("Game");
+        
         SaveManager.LoadGame();
+        Load();
     }
 
     public void OnStartNewGameButtonCkick()
     {
         SaveManager.DeleteSaves();
         SaveManager.LoadGame();
-        SceneManager.LoadScene("Game");
+        Load();
+        
     }
 
     public void OnExitButtonClick()
@@ -43,4 +48,34 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Exit pressed!");
     }
+
+    private void Load()
+    {
+        //SceneManager.LoadScene("Game");
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+        //asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            loadingProgressBar.GetComponent<Slider>().value = asyncLoad.progress;
+            /*if(asyncLoad.progress > 0.9f && !asyncLoad.allowSceneActivation)
+            {
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+                }
+                asyncLoad.allowSceneActivation = true;
+            }*/
+            yield return null;
+        }
+
+        loadingScreen.SetActive(false);
+    }
+
 }
